e43da62668694e15257f2a90eafaa2af3f4e230f
Marina <marina.lopez@king.com>
2025-06-22 18:59:16 +0200

    docs: :memo: Add nvmrc file and how to run in README.

---
+++ README.md
@@ -0,0 +1,26 @@
+## Introduction
+
+This is a personal backend project used for practice.
+It is based in NodeJS and Express and implements a clean architecture.
+
+## How to run
+
+### Use the correct node version
+
+Run the following command to use the [correct node version](.nvmrc).
+
+```bash
+nvm use
+```
+
+### Install dependencies
+
+```bash
+npm install
+```
+
+### Run the application
+
+```bash
+npm run dev
+```
--- src/infrastructure/repositories/auth.repository.impl.ts
+++ src/infrastructure/repositories/auth.repository.impl.ts
@@ -1,9 +1,5 @@
 import { AuthDataSource, AuthRepository, LoginUserDto, RegisterUserDto, UserEntity } from '../../domain';
 
-// Usamos el repositorio como capa, para desacoplar el datasource.
-// Podemos usar diferente origen de datos (mongo, oracle...).
-// Interactuamos con el repositorio y le enviamos el datasource que queramos
-
 export class AuthRepositoryImpl implements AuthRepository {
   constructor(private readonly authDataSource: AuthDataSource) {}
 
